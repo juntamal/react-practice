5ec2f29b2ddc542f5cb86cb1e98004f6a1790244
Junki Yamada <fogg.smiths@gmail.com>
2019-06-03 17:38:02 +0900

    Implement CounterComponent in Redux

--- src/components/ReduxPractice/App.js
+++ src/components/ReduxPractice/App.js
@@ -1,7 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 
-export default class App extends Component {
-  render() {
-    return <h2>hello</h2>;
-  }
+const App = ({ number, plus, minus }) => {
+  return (
+    <div>
+      <h2>{number}</h2>
+      <button onClick={() => {plus(10);}}>+10</button>
+      <button onClick={() => {minus(10);}}>-10</button>
+    </div>
+  );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    number: state,
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    plus: (num) => {
+      dispatch({ type: 'PLUS', payload: { num: num } });
+    },
+    minus: num => {
+      dispatch({ type: 'MINUS', payload: { num: num } });
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
